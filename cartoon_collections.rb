8158c89e525fa74bbc3092808ba7e4e4c21ec861
Andy Del Valle <andydelvalle89@gmail.com>
2020-04-09 20:50:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   planeteer_calls.find do |element|
-    puts valid_calls.include? element
+    valid_calls.include? element
   end
 end
